86e2074bbf95ddc214803e1b2ac11124acca7d2c
dennis-phillips <dennis_phillips@outlook.com>
2020-06-02 12:18:47 -0400

    new changes

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -7,7 +7,7 @@ namespace StringExercises
         static void Main(string[] args)
         {
             string myString = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
-            string[] newArray = myString.Split(" ");
+            string[] newArray = myString.Split(".");
             Console.WriteLine(string.Join(",", newArray));
             //Console.WriteLine("Hello Strings!");
         }
