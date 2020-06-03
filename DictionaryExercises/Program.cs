1d3b2c413542682250bb427c30c1731b59e434a3
dennis-phillips <dennis_phillips@outlook.com>
2020-06-03 07:07:40 -0400

    Unit2 Exercises Complete

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,30 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<double, string> GradebookDictionary = new Dictionary<double, string>();
+            GradebookDictionary.Add(123, "Daryn");
+            GradebookDictionary.Add(456, "Dave");
+            GradebookDictionary.Add(789, "Miguel");
+            GradebookDictionary.Add(111, "Jed");
+            GradebookDictionary.Add(777, "Moe");
+            
+            Console.Write("Enter a Student ID:");
+            int studentID = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter a Student name:");
+            string studentName = Console.ReadLine();
+            if (studentID != 0 && studentName != "")
+            {
+                GradebookDictionary.Add(studentID, studentName);
+            }
+            foreach (KeyValuePair<double, string> id in GradebookDictionary)
+            {
+                Console.WriteLine(id.Key + "." + "-" + id.Value);
+            }
+            {
+
+            }
+            
+            
         }
     }
 }
--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace ListExercises
 {
     class Program
     {
+        public static void numLetters(List<string> words)
+        {
+            Console.Write("Enter a word length:");
+            int userDefinedLength = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (words[i].Length == userDefinedLength)
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+            
+                
+                
+
+            
+        }
+        public static int EvenNums(List<int> nums){
+            int totalSumEvens = 0;
+         foreach (int num in nums)
+	     {
+           if(num % 2 == 0){
+           totalSumEvens+= num;
+
+           } 
+	     }
+            
+            return totalSumEvens;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+            List<int> numbers = new List<int>{1,2,3,4,5,6,7,8,9,10};
+            Console.WriteLine(EvenNums(numbers));
+            List<string> wordsList = new List<string>{"stuff","things","words","this","that"};
+            numLetters(wordsList);
         }
     }
 }
